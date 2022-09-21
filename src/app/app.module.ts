9adc44e447e7e355a54dd7af8c1f9ee4680ca1e3
jahnavi <jahnavi.patel@kodemtechnologies.com>
2022-09-21 12:00:00 +0530

    profile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,40 @@ import { AppComponent } from './app.component';
 import {HttpClientModule} from "@angular/common/http";
 import { LoginComponent } from './onboarding/login/login.component';
 import {FormsModule} from "@angular/forms";
+import { WelcomeComponent } from './home/welcome/welcome.component';
+import { AppRoutingModule } from './app-routing.module';
+import { SignupComponent } from './onboarding/signup/signup.component';
+import { RegisterComponent } from './user/register/register.component';
+import { CommonComponent } from './onboarding/common/common.component';
+import { EducationComponent } from './user/education/education.component';
+import { ExperienceComponent } from './user/experience/experience.component';
+import { SkillsComponent } from './user/skills/skills.component';
+import { SchoolComponent } from './school/school.component';
+import { OrganisationComponent } from './organisation/organisation.component';
+import { EditComponent } from './user/edit/edit.component';
+import { AddressComponent } from './address/address.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    WelcomeComponent,
+    SignupComponent,
+    RegisterComponent,
+    CommonComponent,
+    EducationComponent,
+    ExperienceComponent,
+    SkillsComponent,
+    SchoolComponent,
+    OrganisationComponent,
+    EditComponent,
+    AddressComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
